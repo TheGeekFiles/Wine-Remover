1b8818b5a5a851b15642b545990bb7802c87b772
wordman2577 <67689332+wordman2577@users.noreply.github.com>
2020-08-22 14:55:22 +1000

    Created main file.

---
+++ Wine-Remover.sh
@@ -0,0 +1,1 @@
+sudo apt purge wine-installer wine-stable && sudo apt autoremove && sudo rm -rf ~/.wine/* && sudo rm -rf /usr/lib/wine/* && sudo rm -rf ~/.config/menus/applications-merged/* && sudo rm -rf ~/.config/menus/cinnamon-applications-merged/*
