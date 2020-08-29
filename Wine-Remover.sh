8f1051b8bf43abf7fbf7da48e882a105236fe0a5
wordman2577 <67689332+wordman2577@users.noreply.github.com>
2020-08-29 15:52:30 +1000

    Removed @ECHO from script.

--- Wine-Remover.sh
+++ Wine-Remover.sh
@@ -1,4 +1,3 @@
-@ECHO off
 echo 'Removing Wine from your system...'
 sudo apt purge wine-installer wine-stable && sudo apt autoremove && sudo rm -rf ~/.wine/* && sudo rm -rf /usr/lib/wine/* && sudo rm -rf ~/.config/menus/applications-merged/* && sudo rm -rf ~/.config/menus/cinnamon-applications-merged/*
 echo 'Success!'
